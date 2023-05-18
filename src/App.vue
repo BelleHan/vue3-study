c74588de22cc969417547042548f43a3981a000e
BelleHan <heesu1028@naver.com>
2023-05-18 14:21:44 +0900

    프로젝트 초기화 완료

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="name">{{ name }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = 'Belle Han';
+
+    return {
+      name
+    };
+  }
+}
+</script>
+
+<style>
+  .name{
+    color: blueviolet;
+  }
+</style>

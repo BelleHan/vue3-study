f6119cbbe8c1b4d07f77fc0fb687f93fab1d11f0
BelleHan <heesu1028@naver.com>
2023-05-18 15:51:35 +0900

    양방향 바인딩 (v-model)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div :class="nameClass">
-    {{ name }}
-  </div>
-  <input v-bind:type="type" v-bind:value="name">
+  <input 
+    type="text" 
+    v-model="name"
+  >
   <button 
     class="btn btn-primary"
-    v-on:click="updateName"
+    @click="onSubmit"
   > 
     Click
   </button>
@@ -17,20 +17,14 @@ import { ref } from 'vue';
 export default {
   setup() {
     const name = ref('Belle');
-    const type = ref('number');
-    const nameClass = ref('');
 
-    const updateName = () => {
-      name.value = 'Heesu';
-      type.value = 'text';
-      nameClass.value = 'name';
+    const onSubmit = () => {
+      console.log(name.value)
     };
 
     return {
       name,
-      updateName,
-      type,
-      nameClass,
+      onSubmit,
     };
   }
 }

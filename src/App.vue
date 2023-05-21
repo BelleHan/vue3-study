3a4dae2f3df4fefafaeecd47e411de78bbaa459a
BelleHan <heesu1028@naver.com>
2023-05-22 07:19:50 +0900

    23 computed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="container">
+    <h4>count: {{ count }}</h4>
+    <h4>double count coumputed: {{ doubleCountCoumputed }}</h4> 
+    <h4>double count coumputed: {{ doubleCountCoumputed }}</h4> 
+    <h4>double count method: {{ doubleCountMethod() }}</h4>
+    <h4>double count method: {{ doubleCountMethod() }}</h4>
+    <button @click="count++">Add One</button>
     <h2>To-Do List</h2>
     <TodoSimpleForm @add-todo="addTodo"/>
     
@@ -16,7 +22,7 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue'
 import TodoList from './components/TodoList.vue';
 
@@ -36,22 +42,36 @@ export default {
       todos.value.push(todo);
     };
 
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
     const toggleTodo = (index) => {
       console.log(todos.value[index]);
       todos.value[index].completed = !todos.value[index].completed;
       console.log(todos.value[index]);
     };
 
-    const deleteTodo = (index) => {
-      todos.value.splice(index, 1);
-    };
+    const count = ref(1);
+    const doubleCountCoumputed = computed(() => {
+      console.log('computed');
+      return count.value * 2;
+    });
 
+    const doubleCountMethod = () => {
+      console.log('method');
+      return count.value * 2;
+    };
+ 
     return {
       todos,
       addTodo,
       todoStyle,
       deleteTodo,
       toggleTodo,
+      count,
+      doubleCountCoumputed,
+      doubleCountMethod,
     };
   }
 }

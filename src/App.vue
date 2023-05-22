fd0567c9f6003e6e4a2865fabe175bfb6a40c187
BelleHan <heesu1028@naver.com>
2023-05-22 09:57:30 +0900

    27 async vs sync

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,7 @@ export default {
     const addTodo = (todo) => {
       // 데이터베이스 투두를 저장
       error.value = '';
+      console.log('start');
       axios.post('http://localhost:3000/todos', {
         subject: todo.subject,
         completed: todo.completed,
@@ -51,6 +52,7 @@ export default {
         console.log(err);
         error.value = 'Something went wrong.';
       });
+      console.log('hello');
     };
 
     const deleteTodo = (index) => {

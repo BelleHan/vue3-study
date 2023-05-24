bc4582648103d6c41b99eba84ac2e90c4e4f74d4
BelleHan <heesu1028@naver.com>
2023-05-24 09:04:15 +0900

    46 useRouter로 페이지 이동

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,21 +4,24 @@
         :key="todo.id"
         class="card mt-2"
     >
-        <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-            <input 
-            class="form-check-input"
-            type="checkbox"
-            :checked="todo.completed"
-            @change="toggleTodo(index)"
-            >
-            <label 
-            class="form-check-label"
-            :class="{ todo: todo.completed }"
-            >
-            {{ todo.subject }}
-            </label>
-        </div>
+        <div 
+            class="card-body p-2 d-flex align-items-center"
+            @click="moveToPage(todo.id)"
+        >
+            <div class="form-check flex-grow-1">
+                <input 
+                class="form-check-input"
+                type="checkbox"
+                :checked="todo.completed"
+                @change="toggleTodo(index)"
+                >
+                <label 
+                class="form-check-label"
+                :class="{ todo: todo.completed }"
+                >
+                {{ todo.subject }}
+                </label>
+            </div>
         <div>
             <button 
                 class="btn btn-danger btn-sm"
@@ -32,6 +35,8 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+
 export default {
     props: {
         todos: {
@@ -41,17 +46,30 @@ export default {
     },
     emits: ['toggle-todo', 'delete-todo'],
     setup(props, { emit }) {
+        const router = useRouter();
         const toggleTodo = (index) => {
             emit('toggle-todo', index);
         };
 
         const deleteTodo = (index) => {
             emit('delete-todo', index);
-        }
+        };
+
+        const moveToPage = (todoId) => {
+            console.log(todoId);
+            // router.push('/todos/' + todoId);
+            router.push({
+                name: 'Todo',
+                params: {
+                    id: todoId
+                }
+            });
+        };
 
         return {
             toggleTodo,
             deleteTodo,
+            moveToPage,
         }
     }
 }

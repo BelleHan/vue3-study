2ca6b51a2833717e9659f501cb13123840463b40
BelleHan <heesu1028@naver.com>
2023-05-25 09:46:01 +0900

    To-Do 페이지 컴포넌트 만들기3

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -3,7 +3,10 @@
   <div v-if="loading">
     Loading...
   </div>
-  <form v-else>
+  <form 
+    v-else
+    @submit.prevent="onSave"
+  >
     <div div class="row">
       <div class="col-6">
         <div class="form-group">
@@ -55,10 +58,11 @@ export default {
         const router = useRouter();
         const todo = ref(null);
         const loading = ref(true);
+        const todoId = route.params.id;
 
         const getTodo = async () => {
 
-          const res = await axios.get("http://localhost:3000/todos/" + route.params.id);
+          const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
 
           todo.value = res.data;
           loading.value = false;
@@ -76,11 +80,21 @@ export default {
 
         getTodo();
 
+        const onSave = async () => {
+          const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+            subject: todo.value.subject,
+            completed: todo.value.completed
+          });
+
+          console.log(res);
+        };
+
         return {
           todo,
           loading,
           toggleTodoStatus,
           moveToTodoListPage,
+          onSave,
         };
     }
 

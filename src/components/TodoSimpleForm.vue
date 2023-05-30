ab30309f22d99c783e3f633df87711ca5a7c8d7a
BelleHan <heesu1028@naver.com>
2023-05-30 16:35:33 +0900

    71 getCurrentInstance

--- src/components/TodoSimpleForm.vue
+++ src/components/TodoSimpleForm.vue
@@ -25,10 +25,11 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, getCurrentInstance } from 'vue';
     export default {
         emits: ['add-todo'],
-        setup(props, { emit }) {
+        setup() {
+            const { emit } = getCurrentInstance();
             const todo = ref('');
             const hasError = ref(false);
 

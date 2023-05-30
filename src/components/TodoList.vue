c3141ee22fb78d77523a4a61b81608049541de4a
BelleHan <heesu1028@naver.com>
2023-05-30 11:04:34 +0900

    68 scoped slot 이용하여 리스트 리팩토링

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,37 +1,43 @@
 <template>
-    <div 
+    <!-- <div 
         v-for="(todo, index) in todos"
         :key="todo.id"
         class="card mt-2"
+    > -->
+    <List
+        :items="todos"
     >
-        <div 
-            class="card-body p-2 d-flex align-items-center"
-            style="cursor: pointer"
-            @click="moveToPage(todo.id)"
-        >
-            <div class="flex-grow-1">
-                <input 
-                class="ml-2 mr-2"
-                type="checkbox"
-                :checked="todo.completed"
-                @change="toggleTodo(index, $event)"
-                @click.stop
-                >
-                <span :class="{ todo: todo.completed }"
-                >
-                {{ todo.subject }}
-                </span>
-            </div>
-        <div>
-            <button 
-                class="btn btn-danger btn-sm"
-                @click.stop="openModal(todo.id)"
+        <template #default="{ item, index }">
+            <div 
+                class="card-body p-2 d-flex align-items-center"
+                style="cursor: pointer"
+                @click="moveToPage(item.id)"
             >
-                Delete
-            </button>
-        </div>
-        </div>
-    </div>
+                <div class="flex-grow-1">
+                    <input 
+                    class="ml-2 mr-2"
+                    type="checkbox"
+                    :checked="item.completed"
+                    @change="toggleTodo(index, $event)"
+                    @click.stop
+                    >
+                    <span :class="{ todo: item.completed }"
+                    >
+                    {{ item.subject }}
+                    </span>
+                </div>
+            <div>
+                <button 
+                    class="btn btn-danger btn-sm"
+                    @click.stop="openModal(item.id)"
+                >
+                    Delete
+                </button>
+            </div>
+            </div>
+        </template>
+    </List>
+    <!-- </div> -->
     <teleport to="#modal">
         <Modal 
             v-if="showModal"
@@ -45,10 +51,12 @@
 import { useRouter } from 'vue-router';
 import Modal from '@/components/DeleteModal.vue';
 import { ref } from 'vue';
+import List from '@/components/List.vue';
 
 export default {
     components: {
-        Modal
+        Modal,
+        List
     },
     props: {
         todos: {
